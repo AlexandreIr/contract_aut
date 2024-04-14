965d5dd063afd0761afb830fec47b42d06599168
Alexandre Fernandes <alex.silva250@hotmail.com>
2024-04-13 21:57:09 -0300

    fix: new way date

--- src/model/services/ContractService.java
+++ src/model/services/ContractService.java
@@ -21,13 +21,13 @@ public class ContractService {
 		this.onlinePaymentService = onlinePaymentService;
 	}
 	public void processContract(Contract contract, Integer months) {
-		LocalDate dueDate = contract.getDate();
+		
 
 		double contractInstallment = contract.getTotalValue() / months;
 		for (int i = 1; i <= months; i++) {
 			double contractWithFee = onlinePaymentService.paymentFee(contractInstallment);
 			double contractWithInterest = onlinePaymentService.interest(contractWithFee, i);
-			dueDate = dueDate.plusMonths(1);
+			LocalDate dueDate = contract.getDate().plusMonths(i);
 			Installment isnt = new Installment(dueDate, contractWithInterest, i);
 			contract.addInstallment(isnt);
 		}
